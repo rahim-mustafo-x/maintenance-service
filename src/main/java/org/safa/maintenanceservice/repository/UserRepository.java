502dfd8cf9b5a227bb5aa8ddd672597bd65a9a54
rahim-mustafo-x <rahim.mustafo.x@gmail.com>
2026-07-28 02:44:43 +0500

    fixed notnull annotation

--- src/main/java/org/safa/maintenanceservice/repository/UserRepository.java
+++ src/main/java/org/safa/maintenanceservice/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package org.safa.maintenanceservice.repository;
 
 import jakarta.transaction.Transactional;
-import jakarta.validation.constraints.NotNull;
-import org.jspecify.annotations.NonNull;
 import org.safa.maintenanceservice.models.entity.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -18,28 +16,28 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
      * In here we are taking user model via user_name.
      * It is considered to be used only when login formats like UserDetailsService**/
     @Query("select u from UserEntity u where u.username=:username")
-    Optional<UserEntity> findByUsername(@NonNull @Param("username") String username);
+    Optional<UserEntity> findByUsername(@Param("username") String username);
 
     @Query("select u from UserEntity u where u.id = :userId")
     Optional<UserEntity> findByUsernameId(@Param("userId") long userId);
 
     /** we have to add exists by unique things such as username phone number  in order to avoid 403**/
     @Query("select count(u) > 0 from UserEntity u where u.username=:username")
-    boolean existsByUsername(@NonNull @Param("username") String username);
+    boolean existsByUsername(@Param("username") String username);
 
     @Query("select count(u) > 0 from UserEntity u where u.phoneNumber=:phoneNumber")
-    boolean existsByPhoneNumber(@NonNull @Param("phoneNumber") String phoneNumber);
+    boolean existsByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 
     @Modifying
     @Transactional
     @Query("update UserEntity u set u.password=:password where u.id=:userId")
-    void changePassword(@Param("userId") long userId, @NonNull @Param("password") String password);
+    void changePassword(@Param("userId") long userId, @Param("password") String password);
 
     @Query("select u from UserEntity u where u.phoneNumber=:phoneNumber")
-    Optional<UserEntity> findByPhoneNumber(@NotNull @Param("phoneNumber") String phoneNumber);
+    Optional<UserEntity> findByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 
     @Transactional
     @Modifying
     @Query("update UserEntity u set u.fullName=:fullName, u.username=:username, u.phoneNumber=:phoneNumber where u.id=:userId")
-    void updateByUserId(@NotNull @Param("fullName") String fullName, @NotNull @Param("username") String username, @NotNull @Param("phoneNumber") String phoneNumber, @Param("userId")  long userId);
+    void updateByUserId(@Param("fullName") String fullName, @Param("username") String username, @Param("phoneNumber") String phoneNumber, @Param("userId")  long userId);
 }
